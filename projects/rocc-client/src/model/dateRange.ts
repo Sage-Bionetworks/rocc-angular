0eaa7e12cbc6ad767d0e8d48660f8412cd69a4fc
tschaffter <thomas.schaffter@gmail.com>
2021-07-18 17:21:05 -0700

    Update DateRange model with start and end

--- projects/rocc-client/src/model/dateRange.ts
+++ projects/rocc-client/src/model/dateRange.ts
@@ -15,10 +15,10 @@ export interface DateRange {
     /**
      * Start date of the date range
      */
-    startDate: string;
+    start: string;
     /**
      * End date of the date range
      */
-    endDate: string;
+    end: string;
 }
 
